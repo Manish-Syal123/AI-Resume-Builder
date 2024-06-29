f5b55100558ade2ee4f563fe67794a4fc51df9cb
Manish_Syal <manishsyal77@gmail.com>
2024-06-29 17:03:10 +0530

    Project setup + React Routing

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "./components/ui/button";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
+
+export default App;
