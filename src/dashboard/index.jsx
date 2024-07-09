90ae856b1f9953e90dff89449d94f1dacee90044
Manish_Syal <manishsyal77@gmail.com>
2024-07-09 13:06:15 +0530

    Added No resume found icon in dashboard

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -3,9 +3,11 @@ import AddResume from "./components/AddResume";
 import GlobalApi from "./../../service/GlobalApi";
 import { useUser } from "@clerk/clerk-react";
 import ResumeCardItem from "./components/ResumeCardItem";
+import { Rabbit } from "lucide-react";
 
 const Dashboard = () => {
   const [resumeList, setResumeList] = useState([]);
+  const [loading, setLoading] = useState(false);
   const { user } = useUser();
 
   useEffect(() => {
@@ -14,15 +16,18 @@ const Dashboard = () => {
 
   // To get all the Resumes Created by Current loggedIn User
   const GetResumesList = async () => {
+    setLoading(true);
     try {
       await GlobalApi.getUserResumes(
         user?.primaryEmailAddress?.emailAddress
       ).then((resp) => {
         console.log(resp.data);
         setResumeList(resp.data.data);
+        setLoading(false);
       });
     } catch (error) {
       console.log("Error Fetching User ResumeLists: ", error);
+      setLoading(false);
     }
   };
   return (
@@ -31,20 +36,27 @@ const Dashboard = () => {
       <p>Start Creating AI Resume for your next Job Role!</p>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-10 gap-5">
         <AddResume />
-        {resumeList.length > 0
-          ? resumeList.map((resume, index) => (
-              <ResumeCardItem
-                key={index}
-                resume={resume}
-                refreshData={GetResumesList}
-              />
-            ))
-          : [1, 2, 3, 4].map((item, index) => (
-              <div
-                key={index}
-                className="h-[280px] rounded-lg bg-slate-200 animate-pulse"
-              ></div>
-            ))}
+        {loading ? (
+          [1, 2, 3, 4].map((item, index) => (
+            <div
+              key={index}
+              className="h-[280px] rounded-lg bg-slate-200 animate-pulse"
+            ></div>
+          ))
+        ) : resumeList.length > 0 ? (
+          resumeList.map((resume, index) => (
+            <ResumeCardItem
+              key={index}
+              resume={resume}
+              refreshData={GetResumesList}
+            />
+          ))
+        ) : (
+          <div className="flex flex-col justify-center items-center">
+            <Rabbit size={150} />
+            <h2 className="font-bold text-xl">No Resume Found!</h2>
+          </div>
+        )}
       </div>
     </div>
   );
