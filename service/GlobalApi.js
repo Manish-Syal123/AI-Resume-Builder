cdaf845bf92ef813ab618958ea89455dfbab8c42
Manish_Syal <manishsyal77@gmail.com>
2024-07-16 13:26:24 +0530

    Changed the base URL to the deployed Strapi url

--- service/GlobalApi.js
+++ service/GlobalApi.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_STRAPI_API_KEY;
 const axiosClient = axios.create({
-  baseURL: "http://localhost:1337/api/",
+  // baseURL: "http://localhost:1337/api/",
+  baseURL: import.meta.env.VITE_BASE_URL + "/api/",
+
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${API_KEY}`,
